ac4e1f21291352f65d6a176eb3943790762b2e5f
jc-dannhardt <jaciejunk@gmail.com>
2019-10-08 19:33:49 -0400

    add resturant layout

--- app/src/main/java/com/example/cmsc355application/AddRestaurantActivity.java
+++ app/src/main/java/com/example/cmsc355application/AddRestaurantActivity.java
@@ -11,4 +11,6 @@ public class AddRestaurantActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_restaurant);
     }
+
+    //need a done button
 }
