4a85dd12f9c93a8ab7a1cc860799d0e89f092aa5
jc-dannhardt <jaciejunk@gmail.com>
2019-10-08 18:38:42 -0400

    Scene Update

---
+++ app/src/main/java/com/example/cmsc355application/AddRestaurantActivity.java
@@ -0,0 +1,14 @@
+package com.example.cmsc355application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_restaurant);
+    }
+}
---
+++ app/src/main/java/com/example/cmsc355application/ViewRestaurantActivity.java
@@ -0,0 +1,14 @@
+package com.example.cmsc355application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewRestaurantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_restaurant);
+    }
+}
