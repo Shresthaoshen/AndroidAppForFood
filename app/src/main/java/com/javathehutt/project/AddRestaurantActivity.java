29ac333e048689137388e78e0c157e066dcf3306
jc-dannhardt <jaciejunk@gmail.com>
2019-10-10 12:00:49 -0400

    Refactored packages and directories

--- app/src/main/java/com/javathehutt/project/AddRestaurantActivity.java
+++ app/src/main/java/com/javathehutt/project/AddRestaurantActivity.java
@@ -17,29 +17,14 @@ public class AddRestaurantActivity extends AppCompatActivity {
         setContentView(com.javathehutt.project.R.layout.activity_add_restaurant);
     }
 
-    private Restaurant createRestaurant(){
+    private void createRestaurant(){
 
-        TextInputEditText txtTitle = findViewById(com.javathehutt.project.R.id.txtTitle);
-        EditText numRating = findViewById(com.javathehutt.project.R.id.numRating);
-        EditText numPrice = findViewById(com.javathehutt.project.R.id.numPrice);
-        TextInputEditText txtNotes = findViewById(com.javathehutt.project.R.id.txtNotes);
-
-
-        Restaurant newRestaurant = new Restaurant(txtTitle.getText().toString());
-            newRestaurant.setRating(numRating.getInputType());
-            newRestaurant.setPrice(numPrice.getInputType());
-            newRestaurant.setNotes(txtNotes.getTextColors().toString());
-
-        return newRestaurant;
     }
 
     public void clickSubmit (View view) {
-        //Restaurant newRestaurant = createRestaurant(); //THIS IS BROKEN - if you comment it out it works idk what's going on
 
         Intent submitIntent = new Intent(this, MainActivity.class);
-            //submitIntent.putExtra("ResturantCreated", newRestaurant.toString());
             setResult(RESULT_OK, submitIntent);
-
             finish();
     }
 }
