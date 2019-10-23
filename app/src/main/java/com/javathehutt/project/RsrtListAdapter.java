644a7d84dcf26ee23776dde6d6afd865ac6c4bdf
jc-dannhardt <jaciejunk@gmail.com>
2019-10-23 11:53:04 -0400

    rating bar/UI cleaning

--- app/src/main/java/com/javathehutt/project/RsrtListAdapter.java
+++ app/src/main/java/com/javathehutt/project/RsrtListAdapter.java
@@ -1,6 +1,10 @@
 package com.javathehutt.project;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,7 +42,7 @@ public class RsrtListAdapter extends ArrayAdapter<Restaurant> {
 
         //casting numbers to numbers
         double decimalPrice = Double.parseDouble(uiPrice);
-        double decimalRating = Double.parseDouble(uiPrice);
+        double decimalRating = Double.parseDouble(uiRating) * 10;
 
         //format numbers
         String formatPrice  = String.format("%.02f", decimalPrice);
@@ -62,7 +66,31 @@ public class RsrtListAdapter extends ArrayAdapter<Restaurant> {
         //set rating bar amount
         barRating.setProgress(intRating);
 
+        //sets color for rating bar
+        LayerDrawable layerDrawable = (LayerDrawable) barRating.getProgressDrawable();
+        Drawable progressDrawable = layerDrawable.findDrawableByLayerId(android.R.id.progress);
+        progressDrawable.setColorFilter(Color.parseColor(getColor(intRating)), PorterDuff.Mode.SRC_IN);
+
         return cConvertView;
+    }
+
+    //gets color for the rating bar
+    private String getColor (int rating){
+        String color = "#E02121";
+
+        //red to green
+        if (rating <= 10){ color = "#E02121"; }
+        if (10 < rating && rating <= 20){ color = "#E63F1A"; }
+        if (20 < rating && rating <= 30){ color = "#EC5E13"; }
+        if (30 < rating && rating <= 40){ color = "#F27D0D"; }
+        if (40 < rating && rating <= 50){ color = "#F89C06"; }
+        if (50 < rating && rating <= 60){ color = "#FFBB00"; }
+        if (60 < rating && rating <= 70){ color = "#BFBE08"; }
+        if (70 < rating && rating <= 80){ color = "#7FC110"; }
+        if (80 < rating && rating <= 90){ color = "#3FC418"; }
+        if (90 < rating ){ color = "#00C721"; }
+
+        return color;
     }
 
 }
